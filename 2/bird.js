aaebd8eea617d91460d2882bf2da8cfb05c89cad
Karen <karenpenglabs@gmail.com>
2014-02-17 23:33:48 -0500

    first try

--- 2/bird.js
+++ 2/bird.js
@@ -1,13 +1,16 @@
 function Bird() {
-  this.loc = new PVector(width / 2, height / 2);
+  this.loc = new PVector(width / 8, height / 2);
   this.vel = new PVector(0, 0);
   this.acc = new PVector(0, 0);
+  this.radius = 20;
 }
 
 Bird.prototype.move = function () {
   this.loc.add(this.vel);
   this.vel.add(this.acc);
   this.acc.mult(0);
+  this.loc.x = constrain(this.loc.x, 0, width);
+  this.loc.y = constrain(this.loc.y, 0, height);
 };
 
 Bird.prototype.addForce = function (force) {
@@ -16,15 +19,19 @@ Bird.prototype.addForce = function (force) {
 };
 
 Bird.prototype.rotate = function () {
-  var theta;
-  if (this.vel.y < 0) {
-    theta = PI / 6;
-  } else {
-    theta = -PI / 6;
-  }
+  var theta = map(this.vel.y, -10, 10, -PI / 3, PI / 3);
+  theta = constrain(theta, -PI / 4, PI / 4);
   var xTemp = this.loc.x;
   pushMatrix();
   this.loc.x = this.loc.x * cos(theta) - this.loc.y * sin(theta);
-  this.loc.y = xTemp * sin(theta) + this.loc.y * cos（ theta);
-popMatrix();
-}
+  this.loc.y = xTemp * sin(theta) + this.loc.y * cos(theta);
+  popMatrix();
+};
+
+Bird.prototype.show = function () {
+  fill(255);
+  noStroke();
+  triangle(this.loc.x - sqrt(3) / 2 * this.radius, this.loc.y - this.radius,
+    this.loc.x + sqrt(3) / 2 * this.radius, this.loc.y,
+    this.loc.x - sqrt(3) / 2 * this.radius, this.loc.y + this.radius);
+};
--- 2/main.js
+++ 2/main.js
@@ -1,10 +1,25 @@
+var bird;
+
 function setup() {
-  createGraphics(1200, 400);
-  background(255);
+  createGraphics(1200, 600);
   smooth();
-  frameRate(30);
+  bird = new Bird();
 }
 
 function draw() {
+  background(0);
+  var gravity = new PVector(0, 0.4);
+  bird.addForce(gravity);
+  bird.move();
+  //bird.rotate();
+  bird.show();
 
 }
+
+$(window).keydown(function (event) {
+  if (event.which === 38) {
+    console.log("mouse");
+    var fly = new PVector(0, -9);
+    bird.addForce(fly);
+  }
+});
