9a2da8a2dd65ff421c90f07c60f002a3c5d5f936
Karen <karenpenglabs@gmail.com>
2014-02-23 10:38:18 -0500

    wat

--- 3/main.js
+++ 3/main.js
@@ -1,30 +1,42 @@
 var b, s;
 var gravity;
+var mash;
 
 function setup() {
   createGraphics(1200, 700);
   smooth();
-  b = new Ball(width / 2, 100);
-  s = new Spring(width / 2, 10, 100);
-  gravity = new PVector(0, 2);
+  //b = new Ball(width / 2, 100);
+  //s = new Spring(width / 2, 10, 100);
+  //gravity = new PVector(0, 2);
+  mash = new Mash(32);
 }
 
 function draw() {
   background(255);
+  /*
   b.applyForce(gravity);
   s.connect(b);
   s.constrainLength(b, 30, 200);
   b.update();
-  b.drag(mouseX, mouseY);
+  b.drag(e.pageX, e.pageY);
   s.displayLine(b);
   b.render();
   s.view();
+  */
+  mash.update();
+  mash.show();
 }
 
 $(window).mousedown(function (e) {
-  b.click(e.pageX, e.pageY);
+  event.preventDefault();
+  mash.b.forEach(function (item) {
+    item.clicked(e.pageX, e.pageY);
+  });
 });
 
 $(window).mouseup(function (e) {
-  b.stopDragging();
+  event.preventDefault();
+  mash.b.forEach(function (item) {
+    item.stopDragging();
+  });
 });
--- 3/mash.js
+++ 3/mash.js
@@ -1,11 +1,11 @@
-function Ball(x, y) {
+function Ball(x, y, m) {
   this.loc = new PVector(x, y);
   this.vel = new PVector();
   this.acc = new PVector();
   this.dragOffset = new PVector();
-  this.damping = 0.98;
+  this.damping = 0.96;
   this.dragging = false;
-  this.mass = 24;
+  this.mass = m;
 }
 Ball.prototype.update = function () {
   this.vel.add(this.acc);
@@ -31,7 +31,7 @@ Ball.prototype.render = function () {
   ellipse(this.loc.x, this.loc.y, this.mass * 2, this.mass * 2);
 };
 
-Ball.prototype.click = function (x, y) {
+Ball.prototype.clicked = function (x, y) {
   var dis = dist(x, y, this.loc.x, this.loc.y);
   if (dis < this.mass) {
     this.dragging = true;
@@ -55,7 +55,7 @@ Ball.prototype.drag = function (x, y) {
 function Spring(x, y, l) {
   this.anchor = new PVector(x, y);
   this.len = l;
-  this.k = 0.2;
+  this.k = 0.6;
 }
 
 Spring.prototype.connect = function (b) {
@@ -96,4 +96,57 @@ Spring.prototype.displayLine = function (b) {
   strokeWeight(2);
   stroke(0);
   line(b.loc.x, b.loc.y, this.anchor.x, this.anchor.y);
+};
+
+///////////////////////////////////////////////////////////////////
+function Mash(number) {
+  //this.radius = 100;
+  this.b = [];
+  this.s = [];
+  this.n = number;
+  /*
+  for (var theta = 0; theta < 2 * PI; theta += PI / 3) {
+    this.b.push(new Ball(width / 2 + cos(theta) * this.radius, height / 2 + sin(
+      theta) * this.radius));
+    this.s.push(new Spring());
+  }
+  */
+  for (var i = 0; i < this.n; i++) {
+    this.b.push(new Ball(width / 2, height / 2, 10));
+    this.s.push(new Spring(width / 2, height / 2, 100));
+  }
+}
+
+Mash.prototype.update = function () {
+  for (var j = 0; j < this.n; j++) {
+    //var center = new PVector(width / 2, height / 2);
+    var tar = new PVector(cos(j * PI / this.n * 2) * 2, sin(j * PI /
+      this.n * 2) * 2);
+    //var f = PVector.add(center, tar);
+    this.b[j].applyForce(tar);
+    this.s[j].connect(this.b[j]);
+    this.s[j].constrainLength(this.b[j], 30, 200);
+    this.b[j].update();
+    this.b[j].drag(mouseX, mouseY);
+  }
+};
+
+Mash.prototype.show = function () {
+  for (var i = 0; i < this.n; i++) {
+    this.s[i].displayLine(this.b[i]);
+    this.b[i].render();
+    this.s[i].view();
+  }
+  strokeWeight(3);
+  stroke(0);
+  //fill(100);
+  noFill();
+  beginShape();
+  //curveVertex(0, 0);
+  for (var j = 0; j < this.n; j++) {
+    //curveVertex(this.b[j].loc.x, this.b[j].loc.y);
+    vertex(this.b[j].loc.x, this.b[j].loc.y);
+  }
+  //curveVertex(0, 0);
+  endShape(CLOSE);
 };
